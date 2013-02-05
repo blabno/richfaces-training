26c2ea51f5cd983edbed65cc170bdff3667ec40e
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-02-05 12:34:54 +0100

    Album details - title translations

--- src/main/java/pl/itcrowd/tutorials/richfaces/view/AlbumsView.java
+++ src/main/java/pl/itcrowd/tutorials/richfaces/view/AlbumsView.java
@@ -2,19 +2,40 @@ package pl.itcrowd.tutorials.richfaces.view;
 
 import pl.itcrowd.tutorials.richfaces.dao.AlbumDAO;
 import pl.itcrowd.tutorials.richfaces.domain.Album;
+import pl.itcrowd.tutorials.richfaces.domain.AlbumTranslation;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 @ViewScoped
 @ManagedBean
 public class AlbumsView implements Serializable {
 
+    private Album album = new Album();
+
     @ManagedProperty("#{albumDAO}")
     private AlbumDAO albumDAO;
+
+    private List<Locale> locales;
+
+    public AlbumsView()
+    {
+        setupTranslations();
+    }
+
+    public Album getAlbum()
+    {
+        return album;
+    }
 
     public AlbumDAO getAlbumDAO()
     {
@@ -29,5 +50,33 @@ public class AlbumsView implements Serializable {
     public List<Album> getAlbums()
     {
         return albumDAO.getAllAlbums();
+    }
+
+    public List<Locale> getLanguages()
+    {
+        if (locales == null) {
+            locales = new ArrayList<Locale>();
+            final Iterator<Locale> localeIterator = FacesContext.getCurrentInstance().getApplication().getSupportedLocales();
+            while (localeIterator.hasNext()) {
+                locales.add(localeIterator.next());
+            }
+        }
+        return locales;
+    }
+
+    public void save()
+    {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Saving album"));
+    }
+
+    private void setupTranslations()
+    {
+        for (Locale locale : getLanguages()) {
+            final Map<String, AlbumTranslation> translations = album.getTranslations();
+            final String language = locale.getISO3Language();
+            if (!translations.containsKey(language)) {
+                translations.put(language, new AlbumTranslation(album, null, language, null));
+            }
+        }
     }
 }
